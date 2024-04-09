a54467b8e4fa4a3c81d2855f610734daa92a6ae3
bobbyhadz <opmftw@gmail.com>
2024-04-09 06:57:50 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Using a For loop to print the numbers from 1 to 10
+
+# ✅ for loop 1 to 10 (including 10)
+
+for num in range(1, 11):
+    print(num)
+
+# 👇️ [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list(range(1, 11)))
